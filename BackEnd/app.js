3440fab6710c146b77c1b1f1b2d2155aa76785bc
Steffi Job <steffijob96@gmail.com>
2022-09-20 11:11:39 +0530

    added cors

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -6,6 +6,7 @@ const app =  express();
 const port = process.env.port || 3000 //this is used for server port
 const authRoute = require('./routes/auth-route');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 mongoose.connect('mongodb+srv://user_28:user_28@cluster1.cdowv7k.mongodb.net/?retryWrites=true&w=majority', (err)=>{
     if(err){
@@ -16,9 +17,8 @@ mongoose.connect('mongodb+srv://user_28:user_28@cluster1.cdowv7k.mongodb.net/?re
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(bodyParser.json())
-
+app.use(cors())
 app.use('/auth', authRoute);
 app.get('/', (req, res) =>{
     res.send("Welcome to server")
