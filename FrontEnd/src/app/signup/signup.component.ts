3973e165ea9223c226b01c6fa41670f0862be415
Steffi Job <steffijob96@gmail.com>
2022-09-20 16:34:50 +0530

    modified signup form added alert messages

--- FrontEnd/src/app/signup/signup.component.ts
+++ FrontEnd/src/app/signup/signup.component.ts
@@ -9,6 +9,9 @@ import { AuthService } from '../auth.service';
 })
 export class SignupComponent implements OnInit {
   signupForm!: FormGroup;
+  message:string = '';
+  className = 'd-none'
+  isProcess:boolean = false;
   constructor(private fb:FormBuilder,private auth:AuthService) {
     this.signupForm = this.fb.group({
       'fullName':['',Validators.required],
@@ -20,13 +23,24 @@ export class SignupComponent implements OnInit {
   ngOnInit(): void {
   }
   signup(){
+    this.isProcess = true;
     const data = this.signupForm.value;
     delete data['confirm']
     this.auth.signup(data).subscribe(res=>{
-      alert("User registered successfully")
-      
+      if(res.success){
+        this.isProcess = false;
+        this.message = "Account has been created";
+        this.className = 'alert alert-success';
+      }else{
+        this.isProcess = false;
+        this.message = res.message;
+        this.className = 'alert alert-danger';
+      }
+      //this.signupForm.reset();
     }), (err: any) =>{
-        alert(err)
+      this.isProcess = false;
+      this.message = "Server error !";
+      this.className = 'alert alert-success';
     }
 
     }
